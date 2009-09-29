70950367a09f6366832c8ddbc6b5089d4a81b442
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-09-29 17:41:16 +0000

    Duplicated some MATLAB functionality.

--- trunk/degrees_und.cpp
+++ trunk/degrees_und.cpp
@@ -1,5 +1,4 @@
 #include "bct.h"
-#include <gsl/gsl_blas.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
 
@@ -9,12 +8,10 @@
  * degree of the corresponding node.
  */
 gsl_vector* bct::degrees_und(const gsl_matrix* m) {
-	gsl_matrix* bm = binary(m);
-	gsl_vector* degrees = gsl_vector_calloc(bm->size2);
-	for (int i = 0; i < bm->size2; i++) {
-		gsl_vector_const_view column = gsl_matrix_const_column(bm, i);
-		gsl_vector_set(degrees, i, gsl_blas_dasum(&column.vector));
+	gsl_vector* degrees = gsl_vector_calloc(m->size2);
+	for (int i = 0; i < m->size2; i++) {
+		gsl_vector_const_view column = gsl_matrix_const_column(m, i);
+		gsl_vector_set(degrees, i, bct::nnz(&column.vector));
 	}
-	gsl_matrix_free(bm);
 	return degrees;
 }
--- trunk/utility.cpp
+++ trunk/utility.cpp
@@ -33,6 +33,63 @@ gsl_matrix* bct::binary(const gsl_matrix* m) {
 }
 
 /*
+ * Returns the number of nonzero elements in the given vector.
+ */
+int bct::nnz(const gsl_vector* v) {
+	int count = 0;
+	for (int i = 0; i < v->size; i++) {
+		if (std::abs(gsl_vector_get(v, i)) > EPSILON) {
+			count++;
+		}
+	}
+	return count;
+}
+
+/*
+ * Returns the number of nonzero elements in the given matrix.
+ */
+int bct::nnz(const gsl_matrix* m) {
+	int count = 0;
+	for (int i = 0; i < m->size1; i++) {
+		for (int j = 0; j < m->size2; j++) {
+			if (std::abs(gsl_matrix_get(m, i, j)) > EPSILON) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+/*
+ * Returns a vector of indices of nonzero elements in the given vector.
+ */
+gsl_vector* bct::find(const gsl_vector* v) {
+	gsl_vector* nz = gsl_vector_alloc(nnz(v));
+	int pos = 0;
+	for (int i = 0; i < v->size; i++) {
+		if (std::abs(gsl_vector_get(v, i)) > EPSILON) {
+			gsl_vector_set(nz, pos, i);
+			pos++;
+		}
+	}
+	return nz;
+}
+
+/*
+ * Returns a matrix of size (rows->size, columns->size).  Elements are copied
+ * from the specified rows and columns of m.
+ */
+gsl_matrix* bct::submatrix(const gsl_matrix* m, const gsl_vector* rows, const gsl_vector* columns) {
+	gsl_matrix* s = gsl_matrix_alloc(rows->size, columns->size);
+	for (int i = 0; i < rows->size; i++) {
+		for (int j = 0; j < columns->size; j++) {
+			gsl_matrix_set(s, i, j, gsl_matrix_get(m, gsl_vector_get(rows, i), gsl_vector_get(columns, j)));
+		}
+	}
+	return s;
+}
+
+/*
  * Prints a vector using the given format for each element.  This is only
  * provided for debugging purposes.  In other cases, use gsl_vector_fprintf.
  */
