4fc38b069b8d599fab5c8c2a61ba8e6ce7919035
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-09-29 16:47:08 +0000

    Consolidated utility functions into a single file.

--- Makefile
+++ Makefile
@@ -1,23 +1,14 @@
 objects = \
-	binary_matrix.o \
-	binary_vector.o \
 	degrees_dir.o \
 	degrees_und.o \
-	matrix_printf.o \
-	vector_printf.o
+	utility.o
 bct: $(objects)
 	libtool -static -o libbct.a $(objects)
-binary_matrix.o:
-	cc -c binary_matrix.cpp
-binary_vector.o:
-	cc -c binary_vector.cpp
 degrees_dir.o:
 	cc -c degrees_dir.cpp
 degrees_und.o:
 	cc -c degrees_und.cpp
-matrix_printf.o:
-	cc -c matrix_printf.cpp
-vector_printf.o:
-	cc -c vector_printf.cpp
+utility.o:
+	cc -c utility.cpp
 clean:
 	-rm libbct.a $(objects)
---
+++ utility.cpp
@@ -0,0 +1,58 @@
+#include "bct.h"
+#include <cstdio>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+
+/*
+ * Returns a binary copy of the given vector.
+ */
+gsl_vector* bct::binary_vector(const gsl_vector* v) {
+	gsl_vector* bv = gsl_vector_calloc(v->size);
+	for (int i = 0; i < v->size; i++) {
+		if (abs(gsl_vector_get(v, i)) > EPSILON) {
+			gsl_vector_set(bv, i, 1.0);
+		}
+	}
+	return bv;
+}
+
+/*
+ * Returns a binary copy of the given matrix.
+ */
+gsl_matrix* bct::binary_matrix(const gsl_matrix* m) {
+	gsl_matrix* bm = gsl_matrix_calloc(m->size1, m->size2);
+	for (int i = 0; i < m->size1; i++) {
+		for (int j = 0; j < m->size2; j++) {
+			if (abs(gsl_matrix_get(m, i, j)) > EPSILON) {
+				gsl_matrix_set(bm, i, j, 1.0);
+			}
+		}
+	}
+	return bm;
+}
+
+/*
+ * Prints a vector using the given format for each element.  This is only
+ * provided for debugging purposes.  In other cases, use gsl_vector_fprintf.
+ */
+void bct::vector_printf(const gsl_vector* v, const char* format) {
+	for (int i = 0; i < v->size; i++) {
+		std::printf(format, gsl_vector_get(v, i));
+		std::printf(" ");
+	}
+	std::printf("\n");
+}
+
+/*
+ * Prints a matrix using the given format for each element.  This is only
+ * provided for debugging purposes.  In other cases, use gsl_matrix_fprintf.
+ */
+void bct::matrix_printf(const gsl_matrix* m, const char* format) {
+	for (int i = 0; i < m->size1; i++) {
+		for (int j = 0; j < m->size2; j++) {
+			std::printf(format, gsl_matrix_get(m, i, j));
+			std::printf(" ");
+		}
+		std::printf("\n");
+	}
+}
