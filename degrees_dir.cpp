61fbefd8e501acc824097bc31ebc9f8b9b3d5e1d
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-10-19 08:33:32 +0000

    Condensed degrees_dir into a single function.

--- degrees_dir.cpp
+++ degrees_dir.cpp
@@ -4,40 +4,50 @@
 #include <gsl/gsl_vector.h>
 
 /*
- * Computes the in-degree for each node in a directed binary matrix.  Weights
- * are discarded.  Results are returned in a vector where each element is the
- * in-degree of the corresponding node.
- */
-gsl_vector* bct::degrees_dir_in(const gsl_matrix* m) {
-
-	// Calculated the same way as degrees_und
-	return degrees_und(m);
-}
-
-/*
- * Computes the out-degree for each node in a directed binary matrix.  Weights
- * are discarded.  Results are returned in a vector where each element is the
- * out-degree of the corresponding node.
- */
-gsl_vector* bct::degrees_dir_out(const gsl_matrix* m) {
-	gsl_vector* out_degrees = gsl_vector_calloc(m->size1);
-	for (int i = 0; i < m->size1; i++) {
-		gsl_vector_const_view row = gsl_matrix_const_row(m, i);
-		gsl_vector_set(out_degrees, i, bct::nnz(&row.vector));
-	}
-	return out_degrees;
-}
-
-/*
  * Computes the degree for each node in a directed binary matrix.  Weights are
  * discarded.  Results are returned in a vector where each element is the degree
- * of the corresponding node.
+ * of the corresponding node.  In- and out-degree may be obtained by passing
+ * pointers to appropriately sized vectors for the last two arguments.
  */
-gsl_vector* bct::degrees_dir(const gsl_matrix* m) {
+gsl_vector* bct::degrees_dir(const gsl_matrix* m, gsl_vector* in_degrees, gsl_vector* out_degrees) {
 	assert(m->size1 == m->size2);
-	gsl_vector* in_degrees = degrees_dir_in(m);
-	gsl_vector* out_degrees = degrees_dir_out(m);
-	gsl_vector_add(in_degrees, out_degrees);
-	gsl_vector_free(out_degrees);
-	return in_degrees;
+	
+	// Allocate storage for in- and out-degree vectors if necessary
+	bool free_in_degrees = false;
+	bool free_out_degrees = false;
+	if (in_degrees == NULL) {
+		free_in_degrees = true;
+		in_degrees = gsl_vector_alloc(m->size2);
+	}
+	if (out_degrees == NULL) {
+		free_out_degrees = true;
+		out_degrees = gsl_vector_alloc(m->size1);
+	}
+	
+	// Calculate in-degree
+	for (int i = 0; i < m->size2; i++) {
+		gsl_vector_const_view column = gsl_matrix_const_column(m, i);
+		gsl_vector_set(in_degrees, i, nnz(&column.vector));
+	}
+	
+	// Calculate out-degree
+	for (int i = 0; i < m->size1; i++) {
+		gsl_vector_const_view row = gsl_matrix_const_row(m, i);
+		gsl_vector_set(out_degrees, i, nnz(&row.vector));
+	}
+	
+	// Sum in- and out-degree to get total degree
+	gsl_vector* degrees = gsl_vector_calloc(m->size1);
+	gsl_vector_add(degrees, in_degrees);
+	gsl_vector_add(degrees, out_degrees);
+	
+	// Free in- and out-degree vectors if necessary
+	if (free_in_degrees) {
+		gsl_vector_free(in_degrees);
+	}
+	if (free_out_degrees) {
+		gsl_vector_free(out_degrees);
+	}
+	
+	return degrees;
 }
