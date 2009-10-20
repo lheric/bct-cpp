09044897198b07daa49e7dcab2a7522a14d58808
santosh.manicka <santosh.manicka@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-10-20 20:23:03 +0000

    modified according to bct function return standards

--- matching_ind.cpp
+++ matching_ind.cpp
@@ -15,62 +15,18 @@
  * All output matrices are calculated for upper triangular only (symmetrical).
  */
 
-bct::matching_ind_outputs bct::matching_ind(const gsl_matrix* m) {
+//Compare all (incoming+outgoing) connections
+gsl_matrix* bct::matching_ind(const gsl_matrix* m) {
 	int N, ncon;
 	double matchindex;
-	gsl_matrix* Min;
-	gsl_matrix* Mout;
 	gsl_matrix* Mall;
 	gsl_vector* use;
-
+	
 	N = m->size1;
-	
-	//Compare the incoming connections only
-	Min = gsl_matrix_calloc(N, N);
-	for(int i = 0;i < (N-1);i++)
-		for(int j = (i+1);j < N;j++) {
-			gsl_vector_const_view c1 = gsl_matrix_const_column(m, i);
-			gsl_vector_const_view c2 = gsl_matrix_const_column(m, j);
-			use = logical_not(logical_and(logical_not(&c1.vector), logical_not(&c2.vector)));
-			gsl_vector_set(use, i, 0);
-			gsl_vector_set(use, j, 0);
-			ncon = sum(pick_cells(&c1.vector, use)) + sum(pick_cells(&c2.vector, use));
-			if(ncon == 0)
-				gsl_matrix_set(Min, i, j, 0.0);
-			else
-			{
-				//Improve this by storing the pick_cells results, also calculated above?
-				matchindex = ((double)sum(logical_and(pick_cells(&c1.vector, use), pick_cells(&c2.vector, use))))/ncon;
-				matchindex *= 2.0;
-				gsl_matrix_set(Min, i, j, matchindex);
-			}
-		}
-	
-	//Compare the outgoing connections only
-	Mout = gsl_matrix_calloc(N, N);
-	for(int i = 0;i < (N-1);i++)
-		for(int j = (i+1);j < N;j++) {
-			gsl_vector_const_view r1 = gsl_matrix_const_row(m, i);
-			gsl_vector_const_view r2 = gsl_matrix_const_row(m, j);
-			use = logical_not(logical_and(logical_not(&r1.vector), logical_not(&r2.vector)));
-			gsl_vector_set(use, i, 0);
-			gsl_vector_set(use, j, 0);
-			ncon = sum(pick_cells(&r1.vector, use)) + sum(pick_cells(&r2.vector, use));
-			if(ncon == 0)
-				gsl_matrix_set(Mout, i, j, 0.0);
-			else
-			{
-				//Improve this by storing the pick_cells results, also calculated above?
-				matchindex = ((double)sum(logical_and(pick_cells(&r1.vector, use), pick_cells(&r2.vector, use))))/ncon;
-				matchindex *= 2.0;
-				gsl_matrix_set(Mout, i, j, matchindex);
-			}
-		}
-	
-	//Compare all (incoming+outgoing) connections
 	Mall = gsl_matrix_calloc(N, N);
-	for(int i = 0;i < (N-1);i++)
+	for(int i = 0;i < (N-1);i++) {
 		for(int j = (i+1);j < N;j++) {
+			//c1 and r1 can be moved outside the inner 'for' loop
 			gsl_vector_const_view c1 = gsl_matrix_const_column(m, i);
 			gsl_vector_const_view c2 = gsl_matrix_const_column(m, j);
 			gsl_vector_const_view r1 = gsl_matrix_const_row(m, i);
@@ -93,11 +49,70 @@ bct::matching_ind_outputs bct::matching_ind(const gsl_matrix* m) {
 				gsl_matrix_set(Mall, i, j, matchindex);
 			}
 		}
-	matching_ind_outputs matching_inds;
-	matching_inds.Min = Min;
-	matching_inds.Mout = Mout;
-	matching_inds.Mall = Mall;
-	return matching_inds;
+	}
+	return Mall;
+}
+
+//Compare the incoming connections only
+gsl_matrix* bct::matching_ind_in(const gsl_matrix* m) {
+	int N, ncon;
+	double matchindex;
+	gsl_matrix* Min;
+	gsl_vector* use;
+	
+	N = m->size1;
+	Min = gsl_matrix_calloc(N, N);
+	for(int i = 0;i < (N-1);i++) {
+		for(int j = (i+1);j < N;j++) {
+			gsl_vector_const_view c1 = gsl_matrix_const_column(m, i);
+			gsl_vector_const_view c2 = gsl_matrix_const_column(m, j);
+			use = logical_not(logical_and(logical_not(&c1.vector), logical_not(&c2.vector)));
+			gsl_vector_set(use, i, 0);
+			gsl_vector_set(use, j, 0);
+			ncon = sum(pick_cells(&c1.vector, use)) + sum(pick_cells(&c2.vector, use));
+			if(ncon == 0)
+				gsl_matrix_set(Min, i, j, 0.0);
+			else
+			{
+				//Improve this by storing the pick_cells results, also calculated above?
+				matchindex = ((double)sum(logical_and(pick_cells(&c1.vector, use), pick_cells(&c2.vector, use))))/ncon;
+				matchindex *= 2.0;
+				gsl_matrix_set(Min, i, j, matchindex);
+			}
+		}
+	}
+	return Min;	
+}
+
+//Compare the outgoing connections only
+gsl_matrix* bct::matching_ind_out(const gsl_matrix* m) {
+	int N, ncon;
+	double matchindex;
+	gsl_matrix* Mout;
+	gsl_vector* use;
+	
+	N = m->size1;
+	Mout = gsl_matrix_calloc(N, N);
+	for(int i = 0;i < (N-1);i++) {
+		for(int j = (i+1);j < N;j++) {
+			gsl_vector_const_view r1 = gsl_matrix_const_row(m, i);
+			gsl_vector_const_view r2 = gsl_matrix_const_row(m, j);
+			use = logical_not(logical_and(logical_not(&r1.vector), logical_not(&r2.vector)));
+			gsl_vector_set(use, i, 0);
+			gsl_vector_set(use, j, 0);
+			ncon = sum(pick_cells(&r1.vector, use)) + sum(pick_cells(&r2.vector, use));
+			if(ncon == 0)
+				gsl_matrix_set(Mout, i, j, 0.0);
+			else
+			{
+				//Improve this by storing the pick_cells results, also calculated above?
+				matchindex = ((double)sum(logical_and(pick_cells(&r1.vector, use), pick_cells(&r2.vector, use))))/ncon;
+				matchindex *= 2.0;
+				gsl_matrix_set(Mout, i, j, matchindex);
+			}
+		}
+	}
+	return Mout;
 }
 		
 	
