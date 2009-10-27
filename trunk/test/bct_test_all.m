59bd405259faafc087d8a25fed60baf0b6340110
santosh.manicka <santosh.manicka@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-10-27 18:01:20 +0000

    updated bct_test_all.m with chanegs to jdegree, matching_ind and assortativity

--- trunk/test/bct_test_all.m
+++ trunk/test/bct_test_all.m
@@ -20,22 +20,19 @@ mname = {"cat_all" "cat_ctx" "fve30" "fve32" "macaque47" "macaque71"};
 global failures
 failures = 0;
 
-% TODO: C++ code is causing GSL errors
-% assortativity
-%for i = 1:size(m)(2)
-%	bct_test(sprintf("assortativity_dir %s", mname{i}), assortativity(m{i}, 1) == assortativity_dir_cpp(m{i}))
-%	bct_test(sprintf("assortativity_und %s", mname{i}), assortativity(m{i}, 0) == assortativity_und_cpp(m{i}))
-%endfor
-
 % clustering_coef_bu
 for i = 1:size(m)(2)
 	bct_test(sprintf("clustering_coef_bu %s", mname{i}), all(clustering_coef_bu(m{i}) == clustering_coef_bu_cpp(m{i})'))
 endfor
 
-% degrees_dir
+% degrees_dir -- NEEDS CHANGES
 for i = 1:size(m)(2)
+	%id_cpp = zeros(size(m{i},1));
+	%od_cpp = zeros(size(m{i},1));
 	[id od deg] = degrees_dir(m{i});
 	bct_test(sprintf("degrees_dir %s", mname{i}), all(deg == degrees_dir_cpp(m{i})))
+	%bct_test(sprintf("degrees_dir %s", mname{i}), all(id == ids))
+	%bct_test(sprintf("degrees_dir %s", mname{i}), all(od == ods))
 endfor
 
 % degrees_und
@@ -53,33 +50,36 @@ for i = 1:size(m)(2)
 	bct_test(sprintf("density_und %s", mname{i}), density_und(m{i}) == density_und_cpp(m{i}))
 endfor
 
+% strengths_und
+for i = 1:size(m)(2)
+	bct_test(sprintf("strenghts_und %s", mname{i}), all(strengths_und(m{i}) == strengths_und_cpp(m{i})))
+endfor
+
+% strengths_dir -- NEEDS CHANGES
+for i = 1:size(m)(2)
+	[is, os, str] = strengths_dir(m{i});
+	bct_test(sprintf("strenghts_dir %s", mname{i}), all(str == strengths_dir_cpp(m{i})))
+endfor
+
 % jdegree
 for i = 1:size(m)(2)
-	[J J_od J_id J_bl] = jdegree(m{i});
+	[J,J_od,J_id,J_bl] = jdegree(m{i});
 	bct_test(sprintf("jdegree %s", mname{i}), all(J == jdegree_cpp(m{i})))
-% TODO: Add wrappers for these if we're going to test them
-%	bct_test(sprintf("jdegree_id %s", mname{i}), J_id == jdegree_id_cpp(J))
-%	bct_test(sprintf("jdegree_od %s", mname{i}), J_od == jdegree_od_cpp(J))
-%	bct_test(sprintf("jdegree_bl %s", mname{i}), J_bl == jdegree_bl_cpp(J))
+	bct_test(sprintf("jdegree %s", mname{i}), all([J_id,J_od,J_bl] == jdegree_id_od_bl_cpp(J)))
 endfor
 
 % matching_ind
-%for i = 1:size(m)(2)
-%	[Min Mout Mall] = matching_ind(m{i});
-%	bct_test(sprintf("matching_ind %s", mname{i}), all(Mall == matching_ind_cpp(m{i})))
-%	bct_test(sprintf("matching_ind_in %s", mname{i}), all(Min == matching_ind_in_cpp(m{i})))
-%	bct_test(sprintf("matching_ind_out %s", mname{i}), all(Mout == matching_ind_out_cpp(m{i})))
-%endfor	
-
-% strengths_dir
 for i = 1:size(m)(2)
-	[is os str] = strengths_dir(m{i});
-	bct_test(sprintf("strengths_dir %s", mname{i}), all(str == strengths_dir_cpp(m{i})))
-endfor
+	[Min,Mout,Mall] = matching_ind(m{i});
+	bct_test(sprintf("matching_ind %s", mname{i}), all(Mall == matching_ind_cpp(m{i})))
+	bct_test(sprintf("matching_ind_in %s", mname{i}), all(Min == matching_ind_in_cpp(m{i})))
+	bct_test(sprintf("matching_ind_out %s", mname{i}), all(Mout == matching_ind_out_cpp(m{i})))
+endfor	
 
-% strengths_und
+% assortativity
 for i = 1:size(m)(2)
-	bct_test(sprintf("strengths_und %s", mname{i}), all(strengths_und(m{i}) == strengths_und_cpp(m{i})))
+	bct_test(sprintf("assortativity %s", mname{i}), assortativity(m{i}, 0) == assortativity_und_cpp(m{i}))
+	bct_test(sprintf("assortativity %s", mname{i}), assortativity(m{i}, 1) == assortativity_dir_cpp(m{i}))
 endfor
 
 printf("Failures: %d\n", failures)
