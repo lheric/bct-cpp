e97c3e427d6854362956dff6124541e77e26200a
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-11-08 12:10:08 +0000

    Added init function (currently just sets GSL error handler).

--- trunk/utility.cpp
+++ trunk/utility.cpp
@@ -30,17 +30,35 @@ gsl_matrix* bct::find(const gsl_matrix* m, int cmprFlag, double cmprVal) {
 }
 
 /*
- * Convenience method to free a gsl_vector.
+ * Catches GSL errors and throws BCT exceptions.
+ */
+void bct::gsl_error_handler(const char* reason, const char* file, int line, int gsl_errno) {
+	if (gsl_errno == GSL_ENOMEM) {
+		throw out_of_memory_exception();
+	} else {
+		throw gsl_exception();
+	}
+}
+
+/*
+ * Convenience function to free a gsl_vector.
  */
 void bct::gsl_free(gsl_vector* v) {
 	gsl_vector_free(v);
 }
 
 /*
- * Convenience method to free a gsl_matrix.
+ * Convenience function to free a gsl_matrix.
  */
 void bct::gsl_free(gsl_matrix* m) {
 	gsl_matrix_free(m);
+}
+
+/*
+ * Initializes the BCT library for external use.
+ */
+void bct::init() {
+	gsl_set_error_handler(gsl_error_handler);
 }
 
 /* M
