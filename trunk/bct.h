e91d4478d3093c831797e691caeeaa51de3384f1
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2009-12-23 22:52:07 +0000

    Added motif4generate.

--- trunk/bct.h
+++ trunk/bct.h
@@ -62,7 +62,8 @@ namespace bct {
 	void node_and_edge_betweenness_wei(const gsl_matrix*, gsl_vector* = NULL, gsl_matrix* = NULL);
 
 	// Motifs
-	gsl_matrix* motif3generate(gsl_vector* = NULL, gsl_vector* = NULL, gsl_vector* = NULL);
+	gsl_matrix* motif3generate(gsl_vector_long* = NULL, gsl_vector* = NULL, gsl_vector* = NULL);
+	gsl_matrix* motif4generate(gsl_vector_long* = NULL, gsl_vector* = NULL, gsl_vector* = NULL);
 
 	// Modularity and community structure
 
--- trunk/make_motif34lib.cpp
+++ trunk/make_motif34lib.cpp
@@ -8,7 +8,7 @@
 /*
  * Returns the three-node motif library.
  */
-gsl_matrix* bct::motif3generate(gsl_vector* Mn, gsl_vector* ID, gsl_vector* N) {
+gsl_matrix* bct::motif3generate(gsl_vector_long* Mn, gsl_vector* ID, gsl_vector* N) {
 	
 	// n=0;
 	int n = -1;
@@ -138,12 +138,180 @@ gsl_matrix* bct::motif3generate(gsl_vector* Mn, gsl_vector* ID, gsl_vector* N) {
 	// Mn=uint32(sum(repmat(10.^(5:-1:0),size(M,1),1).*M,2));
 	if (Mn != NULL) {
 		for (int i = 0; i < M->size1; i++) {
-			int Mn_value = 0;
+			long Mn_value = 0;
 			for (int j = 0; j < M->size2; j++) {
 				Mn_value *= 10;
-				Mn_value += (int)gsl_matrix_get(M, i, j);
+				Mn_value += (long)gsl_matrix_get(M, i, j);
 			}
-			gsl_vector_set(Mn, i, Mn_value);
+			gsl_vector_long_set(Mn, i, Mn_value);
+		}
+	}
+	
+	return M;
+}
+
+/*
+ * Returns the four-node motif library.
+ */
+gsl_matrix* bct::motif4generate(gsl_vector_long* Mn, gsl_vector* ID, gsl_vector* N) {
+	
+	// n=0;
+	int n = -1;
+	
+	// M=false(3834,12);
+	gsl_matrix* M = gsl_matrix_calloc(3834, 12);
+	
+	// CL=zeros(3834,16,'uint8');
+	gsl_matrix* CL = zeros(3834, 16);
+	
+	// cl=zeros(1,16,'uint8');
+	gsl_vector* cl = gsl_vector_calloc(16);
+	
+	// for i=0:2^12-1
+	for (int i = 0; i < 4096; i++) {
+		
+		// m=dec2bin(i);
+		// m=[num2str(zeros(1,12-length(m)), '%d') m];
+		char m[13];
+		dec2bin(i, 12, m);
+		
+		// G=str2num ([ ...
+		// '0'   ' '  m(4)  ' '  m(7)  ' '  m(10) ;
+		// m(1)  ' '  '0'   ' '  m(8)  ' '  m(11) ;
+		// m(2)  ' '  m(5)  ' '  '0'   ' '  m(12) ;
+		// m(3)  ' '  m(6)  ' '  m(9)  ' '  '0'    ]);
+		gsl_matrix* G = gsl_matrix_calloc(4, 4);
+		for (int j = 0, k = 0; j < 4; j++) {
+			for (int i = 0; i < 4; i++) {
+				if (i != j && m[k++] == '1') {
+					gsl_matrix_set(G, i, j, 1.0);
+				}
+			}
+		}
+		
+		// Gs=G+G.';
+		gsl_matrix* Gs = gsl_matrix_alloc(4, 4);
+		gsl_matrix_transpose_memcpy(Gs, G);
+		gsl_matrix_add(Gs, G);
+		
+		// v=Gs(1,:);
+		gsl_vector* v = gsl_vector_alloc(4);
+		gsl_matrix_get_row(v, Gs, 0);
+		
+		// for j=1:2,
+		for (int j = 1; j <= 2; j++) {
+			
+			// v=any(Gs(v~=0,:),1)+v;
+			gsl_vector* v_nonzero = compare_elements(v, cmp_not_equal, 0.0);
+			gsl_vector* column_indices = sequence(0, 3);
+			gsl_matrix* Gs_indexed = log_ord_index(Gs, v_nonzero, column_indices);
+			if (Gs_indexed != NULL) {
+				gsl_vector* any_Gs = any(Gs_indexed, 1);
+				gsl_vector_add(v, any_Gs);
+				gsl_vector_free(any_Gs);
+				gsl_matrix_free(Gs_indexed);
+			}
+			gsl_vector_free(column_indices);
+			gsl_vector_free(v_nonzero);
+		}
+		
+		// if v
+		if (to_bool(v)) {
+			
+			// n=n+1;
+			n++;
+			
+			// G2=(G*G)~=0;
+			gsl_matrix* temp = mul(G, G);
+			gsl_matrix* G2 = compare_elements(temp, cmp_not_equal, 0.0);
+			gsl_matrix_free(temp);
+			
+			// Ko=sum(G,2);
+			gsl_vector* Ko = sum(G, 2);
+			
+			// Ki=sum(G,1).';
+			gsl_vector* Ki = sum(G, 1);
+			
+			// Ko2=sum(G2,2);
+			gsl_vector* Ko2 = sum(G2, 2);
+			
+			// Ki2=sum(G2,1).';
+			gsl_vector* Ki2 = sum(G2, 1);
+			
+			// cl(:)=sortrows([Ki Ko Ki2 Ko2]).';
+			gsl_matrix* Ki_Ko = concatenate_rows(Ki, Ko);
+			gsl_matrix* Ki_Ko_Ki2 = concatenate_rows(Ki_Ko, Ki2);
+			gsl_matrix* Ki_Ko_Ki2_Ko2 = concatenate_rows(Ki_Ko_Ki2, Ko2);
+			gsl_matrix* Ks_sort = sortrows(Ki_Ko_Ki2_Ko2);
+			gsl_matrix* Ks_transpose = gsl_matrix_alloc(4, Ki->size);
+			gsl_matrix_transpose_memcpy(Ks_transpose, Ks_sort);
+			gsl_vector_free(cl);
+			cl = to_vector(Ks_transpose);
+			gsl_matrix_free(Ks_transpose);
+			gsl_matrix_free(Ks_sort);
+			gsl_matrix_free(Ki_Ko_Ki2_Ko2);
+			gsl_matrix_free(Ki_Ko_Ki2);
+			gsl_matrix_free(Ki_Ko);
+			
+			// CL(n,:)=cl;
+			gsl_matrix_set_row(CL, n, cl);
+			
+			// M(n,:)=G([2:5 7:10 12:15]);
+			double indices[] = { 1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14 };
+			gsl_vector_view indices_vv = gsl_vector_view_array(indices, 12);
+			gsl_vector* G_portion = ordinal_index(G, &indices_vv.vector);
+			gsl_matrix_set_row(M, n, G_portion);
+			gsl_vector_free(G_portion);
+		}
+		
+		gsl_vector_free(v);
+		gsl_matrix_free(Gs);
+		gsl_matrix_free(G);
+	}
+	
+	// [u1 u2 ID]=unique(CL,'rows');
+	gsl_vector* temp_ID = gsl_vector_alloc(3834);
+	gsl_matrix* u1 = unique_rows(CL, "last", NULL, temp_ID);
+	gsl_vector_add_constant(temp_ID, 1.0);
+	gsl_matrix_free(u1);
+	gsl_vector_free(cl);
+	gsl_matrix_free(CL);
+	
+	// [X ind]=sortrows(ID);
+	gsl_permutation* ind = gsl_permutation_alloc(3834);
+	gsl_sort_vector_index(ind, temp_ID);  // TODO: Not stable!
+	
+	// ID=ID(ind,:);
+	if (ID == NULL) {
+		gsl_vector_free(temp_ID);
+	} else {
+		gsl_permute_vector(ind, temp_ID);
+		gsl_vector_memcpy(ID, temp_ID);
+		gsl_vector_free(temp_ID);
+	}
+	
+	// M=M(ind,:);
+	gsl_matrix* permute_M = permute_rows(ind, M);
+	gsl_matrix_free(M);
+	M = permute_M;
+	gsl_permutation_free(ind);
+	
+	// N=sum(M,2);
+	if (N != NULL) {
+		gsl_vector* temp_N = sum(M, 2);
+		gsl_vector_memcpy(N, temp_N);
+		gsl_vector_free(temp_N);
+	}
+	
+	// Mn=uint32(sum(repmat(10.^(5:-1:0),size(M,1),1).*M,2));
+	if (Mn != NULL) {
+		for (int i = 0; i < M->size1; i++) {
+			long Mn_value = 0;
+			for (int j = 0; j < M->size2; j++) {
+				Mn_value *= 10;
+				Mn_value += (long)gsl_matrix_get(M, i, j);
+			}
+			gsl_vector_long_set(Mn, i, Mn_value);
 		}
 	}
 	
