5aa7080cd7e521208e5c550cba1fd3381ad64a86
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-02-17 07:10:36 +0000

    Fixed bug in test code for random graph generation.

--- test/bct_test_reference.m
+++ test/bct_test_reference.m
@@ -1,15 +1,16 @@
 bct_test_setup
 
 N = floor(91 * rand()) + 10;
-K = floor((N * N - N + 1) * rand());
+Kdir = floor((N * N - N + 1) * rand());
+Kund = floor(Kdir / 2);
 
 % makerandCIJ_dir
-CIJ = makerandCIJ_dir(N, K);
-bct_test("makerandCIJ_dir", size(CIJ) == [N N] && sum(degrees_dir(CIJ)) == K)
+CIJ = makerandCIJ_dir(N, Kdir);
+bct_test("makerandCIJ_dir", size(CIJ) == [N N] && sum(degrees_dir(CIJ)) == Kdir)
 
 % makerandCIJ_und
-CIJ = makerandCIJ_und(N, K);
-bct_test("makerandCIJ_und", size(CIJ) == [N N] && sum(degrees_und(CIJ)) == 2 * K)
+CIJ = makerandCIJ_und(N, Kund);
+bct_test("makerandCIJ_und", size(CIJ) == [N N] && sum(degrees_und(CIJ)) == 2 * Kund)
 
 ITER = 10;
 
