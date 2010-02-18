dd9afe3f29da9cc19a39b5d1e7bb5da06bbba731
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-02-18 02:06:13 +0000

    Updated graph randomization metrics.

--- trunk/latmio_dir.cpp
+++ trunk/latmio_dir.cpp
@@ -1,160 +1,150 @@
 #include "bct.h"
+#include <cmath>
+#include <ctime>
 #include <gsl/gsl_matrix.h>
-#include <gsl/gsl_math.h>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_vector.h>
 
 /*
- * Returns a 'latticized' graph R, with equivalent degree sequence to the original 
- * weighted directed graph G, and with preserved connectedness 
- * (hence the input graph must be connected).
- *
- * Each edge is rewired (on average) ITER times. The strength distributions 
- * are not preserved for weighted graphs.
- *
- * Rewiring algorithm: Maslov and Sneppen (2002) Science 296:910
- * Latticizing algorithm: Sporns and Zwi (2004); Neuroinformatics 2:145
+ * Returns a latticized graph with equivalent degree sequence to the original
+ * weighted directed graph.  On average, each edge is rewired ITER times.  Out-
+ * strength is preserved for weighted graphs, while in-strength is not.
  */
-
-gsl_matrix* bct::latmio_dir(const gsl_matrix* m, int iters) {
-	//It seems D could also be loaded from a file. How to consider that?
-	gsl_matrix* R = copy(m);
-	int n = m->size1;
-	gsl_matrix* D = zeros(n);
-	gsl_vector* v1 = sequence(1, 1, n-1); 
-	gsl_vector* v2 = sequence(n-1, -1, 1); 
-	gsl_matrix* u1 = concatenate_columns(v1, v2); 
-	gsl_vector* v3 = min(u1); 
-	gsl_vector* u = concatenate(0.0, v3); 
-	gsl_vector_free(v1);
-	gsl_vector_free(v2);
-	gsl_vector_free(v3);
-	gsl_matrix_free(u1);
-	int upper_lim = (int)ceil((n-1)/2);
-	for(int v = 0;v <= upper_lim;v++) {
-		gsl_vector* index1 = sequence((v+1), 1, (n-1)); 
-		gsl_vector* index2 = sequence(0, 1, v); 
-		gsl_vector* u_seg1 = ordinal_index(u, index1); 
-		gsl_vector* u_seg2 = ordinal_index(u, index2); 
-		gsl_vector* seg_splice = concatenate(u_seg1, u_seg2); 
-		gsl_vector_view bottom_row = gsl_matrix_row(D, (n-1-v));
-		gsl_vector_memcpy(&bottom_row.vector, seg_splice);
-		gsl_vector_view top_row = gsl_matrix_row(D, v);
-		gsl_vector* rev_seg_splice = reverse(seg_splice); 
-		gsl_vector_memcpy(&top_row.vector, rev_seg_splice);
-		gsl_vector_free(index1);
-		gsl_vector_free(index2);
-		gsl_vector_free(u_seg1);
-		gsl_vector_free(u_seg2);
-		gsl_vector_free(seg_splice);
-		gsl_vector_free(rev_seg_splice);
-		
-	}
-	gsl_vector_free(u);
+gsl_matrix* bct::latmio_dir(const gsl_matrix* R, int ITER) {
+	if (safe_mode) check_status(R, DIRECTED, "latmio_dir");
+	if (R->size1 != R->size2) throw size_exception();
 	
-	//[i j]=find(R);
-	gsl_matrix* R_ij = find_ij(R); 
-	gsl_vector_view i = gsl_matrix_column(R_ij, 0); 
-	gsl_vector_view j = gsl_matrix_column(R_ij, 1); 
-	int K = R_ij->size1;
-	int tot_iters = iters;
-	tot_iters *= K;
-	int rewire = 0;
-	srand(time(0));
-	for(int iter = 1;iter <= tot_iters;iter++) {
-		while(1) {
-			rewire = 1;
-			int a,b,c,d;
+	gsl_rng_default_seed = std::time(NULL);
+	gsl_rng* rng = gsl_rng_alloc(gsl_rng_default);
+	
+	// n=length(R);
+	int n = length(R);
+	
+	// D=zeros(n);
+	gsl_matrix* D = zeros(n);
+	
+	// u=[0 min([mod(1:n-1,n);mod(n-1:-1:1,n)])];
+	gsl_vector* seq1 = sequence(1, n - 1);
+	gsl_vector* seq2 = sequence(n - 1, -1, 1);
+	gsl_matrix* seq1_seq2 = concatenate_columns(seq1, seq2);
+	gsl_vector_free(seq1);
+	gsl_vector_free(seq2);
+	gsl_vector* min_seq1_seq2 = min(seq1_seq2);
+	gsl_matrix_free(seq1_seq2);
+	gsl_vector* u = concatenate(0.0, min_seq1_seq2);
+	gsl_vector_free(min_seq1_seq2);
+	
+	// for v=1:ceil(n/2)
+	for (int v = 1; v <= (int)std::ceil((double)n / 2.0); v++) {
+		
+		// D(n-v+1,:)=u([v+1:n 1:v]);
+		gsl_vector* u_indices1 = sequence(v, n - 1);
+		gsl_vector* u_indices2 = sequence(0, v - 1);
+		gsl_vector* u_indices = concatenate(u_indices1, u_indices2);
+		gsl_vector_free(u_indices1);
+		gsl_vector_free(u_indices2);
+		gsl_vector* u_idx = ordinal_index(u, u_indices);
+		gsl_vector_free(u_indices);
+		gsl_matrix_set_row(D, n - v, u_idx);
+		gsl_vector_free(u_idx);
+		
+		// D(v,:)=D(n-v+1,n:-1:1);
+		gsl_vector* D_rows = gsl_vector_alloc(1);
+		gsl_vector_set(D_rows, 0, (double)(n - v));
+		gsl_vector* D_cols = sequence(n - 1, -1, 0);
+		gsl_matrix* D_idx = ordinal_index(D, D_rows, D_cols);
+		gsl_vector_free(D_rows);
+		gsl_vector_free(D_cols);
+		gsl_vector* D_idx_v = to_vector(D_idx);
+		gsl_matrix_free(D_idx);
+		gsl_matrix_set_row(D, v - 1, D_idx_v);
+		gsl_vector_free(D_idx_v);
+	}
+	
+	// [i j]=find(R);
+	gsl_matrix* find_R = find_ij(R);
+	gsl_vector_view i = gsl_matrix_column(find_R, 0);
+	gsl_vector_view j = gsl_matrix_column(find_R, 1);
+	
+	// K=length(i);
+	int K = length(&i.vector);
+	
+	// ITER=K*ITER;
+	ITER = K * ITER;
+	
+	gsl_matrix* _R = copy(R);
+	
+	// for iter=1:ITER
+	for (int iter = 1; iter <= ITER; iter++) {
+		
+		// while 1
+		while (true) {
+			
 			int e1, e2;
-			while(1) {
-				e1 = ceil((K-1) * (((double)rand())/((double)RAND_MAX)));
-				e2 = ceil((K-1) * (((double)rand())/((double)RAND_MAX)));
-				while(e2 == e1) {
-					e2 = ceil((K-1) * (((double)rand())/((double)RAND_MAX)));
+			int a, b, c, d;
+			
+			// while 1
+			while (true) {
+				
+				// e1=ceil(K*rand);
+				e1 = gsl_rng_uniform_int(rng, K);
+				
+				// e2=ceil(K*rand);
+				e2 = gsl_rng_uniform_int(rng, K);
+				
+				// while (e2==e1),
+				while (e2 == e1) {
+					
+					// e2=ceil(K*rand);
+					e2 = gsl_rng_uniform_int(rng, K);
 				}
-				a = gsl_vector_get(&i.vector, e1);
-				b = gsl_vector_get(&j.vector, e1);
-				c = gsl_vector_get(&i.vector, e2);
-				d = gsl_vector_get(&j.vector, e2);
-				//if all(a~=[c d]) && all(b~=[c d]);
-				if(a != c && a != d && b!=c && b!=d) {  //all four vertices must be different
+				
+				// a=i(e1); b=j(e1);
+				a = (int)gsl_vector_get(&i.vector, e1);
+				b = (int)gsl_vector_get(&j.vector, e1);
+				
+				// c=i(e2); d=j(e2);
+				c = (int)gsl_vector_get(&i.vector, e2);
+				d = (int)gsl_vector_get(&j.vector, e2);
+				
+				// if all(a~=[c d]) && all(b~=[c d]);
+				if (a != c && a != d && b != c && b != d) {
+					
+					// break
 					break;
 				}
 			}
-	        
-	        //if ~(R(a,d) || R(c,b))
-	        //rewiring condition
-	        if(!(gsl_matrix_get(R, a, d) || gsl_matrix_get(R, c, b))) {
-	        	//if (D(a,b)+D(c,d))>=(D(a,d)+D(c,b))
-	        	double val1 = gsl_matrix_get(D, a, b);
-	        	double val2 = gsl_matrix_get(D, c, d);
-	        	double val3 = gsl_matrix_get(D, a, d);
-	        	double val4 = gsl_matrix_get(D, c, b);
-	        	if((val1 + val2) >= (val3 + val4)) { //lattice condition
-	        		//R(a,d)=R(a,b); R(a,b)=0;
-	    			gsl_matrix_set(R, a, d, gsl_matrix_get(R, a, b));
-	    			gsl_matrix_set(R, a, b, 0.0);
-                	//R(c,b)=R(c,d); R(c,d)=0;
-                	gsl_matrix_set(R, c, b, gsl_matrix_get(R, c, d));
-	    			gsl_matrix_set(R, c, d, 0.0);
-	    			gsl_vector_set(&j.vector, e1, d);
-	    			gsl_vector_set(&j.vector, e2, b);
-	    			break;
-		    	}
+			
+			// if ~(R(a,d) || R(c,b))
+			if (fp_zero(gsl_matrix_get(_R, a, d)) && fp_zero(gsl_matrix_get(_R, c, b))) {
+				
+				// if (D(a,b)+D(c,d))>=(D(a,d)+D(c,b))
+				if (gsl_matrix_get(D, a, b) + gsl_matrix_get(D, c, d) >=
+					gsl_matrix_get(D, a, d) + gsl_matrix_get(D, c, b)) {
+					
+					// R(a,d)=R(a,b); R(a,b)=0;
+					gsl_matrix_set(_R, a, d, gsl_matrix_get(_R, a, b));
+					gsl_matrix_set(_R, a, b, 0.0);
+					
+					// R(c,b)=R(c,d); R(c,d)=0;
+					gsl_matrix_set(_R, c, b, gsl_matrix_get(_R, c, d));
+					gsl_matrix_set(_R, c, d, 0.0);
+					
+					// j(e1) = d;
+					gsl_vector_set(&j.vector, e1, (double)d);
+					
+					// j(e2) = b;
+					gsl_vector_set(&j.vector, e2, (double)b);
+					
+					// break;
+					break;
+				}
 			}
 		}
 	}
-	gsl_matrix_free(R_ij);
-	gsl_matrix_free(D);
-	return R;
-}      	
-	        					
-	        				
-	        	
-	            
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-		
 	
-				
-
-
-
-
-
-
-
-
-
-
+	gsl_rng_free(rng);
+	gsl_matrix_free(D);
+	gsl_matrix_free(find_R);
+	return _R;
+}
--- trunk/test/latmio_und_connected_cpp.cpp
+++ trunk/test/latmio_und_connected_cpp.cpp
@@ -4,17 +4,19 @@
 #include <octave/oct.h>
 
 DEFUN_DLD(latmio_und_connected_cpp, args, , "Wrapper for C++ function.") {
-		if (args.length() == 0) {
-			return octave_value_list();
-		}
-		Matrix m = args(0).matrix_value();
-		int iters = args(1).int_value();
-		//return values
-		if (!error_state) {
-			gsl_matrix* gslm = bct_test::to_gslm(m);
-			gsl_matrix* R = bct::latmio_und_connected(gslm, iters);
-			return octave_value(bct_test::from_gsl(R));
-		} else {
-			return octave_value_list();
-		}
+	if (args.length() != 2) {
+		return octave_value_list();
+	}
+	Matrix R = args(0).matrix_value();
+	int ITER = args(1).int_value();
+	if (!error_state) {
+		gsl_matrix* R_gsl = bct_test::to_gslm(R);
+		gsl_matrix* _R = bct::latmio_und_connected(R_gsl, ITER);
+		octave_value ret = bct_test::from_gsl(_R);
+		gsl_matrix_free(R_gsl);
+		gsl_matrix_free(_R);
+		return ret;
+	} else {
+		return octave_value_list();
+	}
 }
