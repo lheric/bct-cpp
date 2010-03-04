5f60080db71ec67990c61d7ae55b220ae044e3bc
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-03-04 22:38:43 +0000

    Updated weighted NSP to use bct::invert_elements.

--- trunk/norm_avr_shortest_path_length_wei.cpp
+++ trunk/norm_avr_shortest_path_length_wei.cpp
@@ -5,25 +5,17 @@
 /*
  * Computes the normalized average shortest path length for a weighted graph.
  */
-double bct::norm_avr_shortest_path_length_wei(const gsl_matrix* m, double max_weight) {
+double bct::norm_avr_shortest_path_length_wei(const gsl_matrix* m, double wmax) {
 	if (safe_mode) check_status(m, WEIGHTED, "norm_avr_shortest_path_length_wei");
 	if (m->size1 != m->size2) throw size_exception();
 	int N = m->size1;
-	gsl_matrix* _m = gsl_matrix_calloc(N, N);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			double value = gsl_matrix_get(m, i, j);
-			if (fp_nonzero(value)) {
-				gsl_matrix_set(_m, i, j, 1.0 / value);
-			}
-		}
-	}
-	gsl_matrix* D = distance_wei(_m);
-	gsl_matrix_free(_m);
-	double max_distance = (double)N / max_weight;
-	gsl_matrix* D_gt_max_distance = compare_elements(D, fp_greater, max_distance);
-	logical_index_assign(D, D_gt_max_distance, max_distance);
-	gsl_matrix_free(D_gt_max_distance);
+	gsl_matrix* inv_m = invert_elements(m);
+	gsl_matrix* D = distance_wei(inv_m);
+	gsl_matrix_free(inv_m);
+	double dmax = (double)N / wmax;
+	gsl_matrix* D_gt_dmax = compare_elements(D, fp_greater, dmax);
+	logical_index_assign(D, D_gt_dmax, dmax);
+	gsl_matrix_free(D_gt_dmax);
 	double sum = 0.0;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
@@ -31,6 +23,6 @@ double bct::norm_avr_shortest_path_length_wei(const gsl_matrix* m, double max_we
 		}
 	}
 	gsl_matrix_free(D);
-	double min_distance = 1.0 / max_weight;
-	return ((sum / (double)(N * N - N)) - min_distance) / (max_distance - min_distance);
+	double dmin = 1.0 / wmax;
+	return ((sum / (double)(N * N - N)) - dmin) / (dmax - dmin);
 }
