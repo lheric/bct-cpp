bde8f192eef97b654fc7dad295919d8e1bb826cb
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-03-17 15:34:57 +0000

    Split bct::to_undirected into binary and weighted versions.

--- trunk/convert.cpp
+++ trunk/convert.cpp
@@ -52,25 +52,38 @@ gsl_matrix* bct::to_positive(const gsl_matrix* m) {
 }
 
 /*
- * Returns an undirected copy of the given matrix.  For every pair of nodes:
- * - If m(i, j) and m(j, i) are the same, no change is made.
- * - If m(i, j) is zero and m(j, i) is nonzero, m(i, j) is set to m(j, i).
- * - If m(i, j) is nonzero and m(j, i) is zero, m(j, i) is set to m(i, j).
- * - If m(i, j) and m(j, i) are different nonzero values, they are both set to
- *   the average of these values.
+ * Returns an undirected copy of the given binary matrix.  For every pair of
+ * nodes, if either m(i, j) or m(j, i) is nonzero, then both m(i, j) and m(j, i)
+ * are set to one.  Otherwise, both are set to zero.
  */
-gsl_matrix* bct::to_undirected(const gsl_matrix* m) {
+gsl_matrix* bct::to_undirected_bin(const gsl_matrix* m) {
 	if (m->size1 != m->size2) throw size_exception();
-	gsl_matrix* und_m = copy(m);
+	gsl_matrix* und_m = gsl_matrix_calloc(m->size1, m->size2);
 	for (int i = 0; i < (int)m->size1; i++) {
 		for (int j = i; j < (int)m->size2; j++) {
 			double value_ij = gsl_matrix_get(m, i, j);
 			double value_ji = gsl_matrix_get(m, j, i);
-			if (fp_zero(value_ij) && fp_nonzero(value_ji)) {
-				gsl_matrix_set(und_m, i, j, value_ji);
-			} else if (fp_nonzero(value_ij) && fp_zero(value_ji)) {
-				gsl_matrix_set(und_m, j, i, value_ij);
-			} else if (fp_not_equal(value_ij, value_ji)) {
+			if (fp_nonzero(value_ij) || fp_nonzero(value_ji)) {
+				gsl_matrix_set(und_m, i, j, 1.0);
+				gsl_matrix_set(und_m, j, i, 1.0);
+			}
+		}
+	}
+	return und_m;
+}
+
+/*
+ * Returns an undirected copy of the given weighted matrix.  For every pair of
+ * nodes, m(i, j) and m(j, i) are both set to the average of their two values.
+ */
+gsl_matrix* bct::to_undirected_wei(const gsl_matrix* m) {
+	if (m->size1 != m->size2) throw size_exception();
+	gsl_matrix* und_m = gsl_matrix_calloc(m->size1, m->size2);
+	for (int i = 0; i < (int)m->size1; i++) {
+		for (int j = i; j < (int)m->size2; j++) {
+			double value_ij = gsl_matrix_get(m, i, j);
+			double value_ji = gsl_matrix_get(m, j, i);
+			if (fp_nonzero(value_ij) || fp_nonzero(value_ji)) {
 				double average = (value_ij + value_ji) / 2.0;
 				gsl_matrix_set(und_m, i, j, average);
 				gsl_matrix_set(und_m, j, i, average);
