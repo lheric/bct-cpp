9e64829fde8cd1ba9a6803e184bb79a2d78357fb
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-03-17 16:48:57 +0000

    Updated efficient weight-preserving versions of bct::makerandCIJ.

--- trunk/makerandCIJ_wd.cpp
+++ trunk/makerandCIJ_wd.cpp
@@ -3,10 +3,9 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_permutation.h>
 #include <gsl/gsl_permute_vector.h>
+#include <gsl/gsl_randist.h>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_vector.h>
-
-gsl_matrix* makerandCIJ_wd(int, const gsl_vector*);
 
 /*
  * Generates a random directed weighted graph with N nodes and K edges.  Weights
@@ -17,33 +16,10 @@ gsl_matrix* bct::makerandCIJ_wd(int N, int K, double wmin, double wmax) {
 	gsl_rng_default_seed = std::time(NULL);
 	gsl_rng* rng = gsl_rng_alloc(gsl_rng_default);
 	gsl_vector* w = gsl_vector_alloc(K);
-	for (int x = 0; x < K; x++) {
-		gsl_vector_set(w, x, gsl_rng_uniform(rng) * (wmax - wmin) + wmin);
+	for (int i = 0; i < K; i++) {
+		gsl_vector_set(w, i, gsl_rng_uniform(rng) * (wmax - wmin) + wmin);
 	}
 	gsl_rng_free(rng);
-	gsl_matrix* ret = makerandCIJ_wd(N, w);
-	gsl_vector_free(w);
-	return ret;
-}
-
-/*
- * Generates a random directed weighted graph with the same number of nodes,
- * number of edges, and weight distribution as the given graph.  No edges are
- * placed on the main diagonal.  The given matrix should therefore not contain
- * nonzero entries on the main diagonal.
- */
-gsl_matrix* bct::makerandCIJ_wd(const gsl_matrix* m) {
-	int N = m->size1;
-	gsl_vector* w = nonzeros(m);
-	gsl_matrix* ret = makerandCIJ_wd(N, w);
-	gsl_vector_free(w);
-	return ret;
-}
-
-gsl_matrix* makerandCIJ_wd(int N, const gsl_vector* w) {
-	using namespace bct;
-	
-	int K = w->size;
 	
 	// ind = ~eye(N);
 	gsl_matrix* eye_N = eye_double(N);
@@ -68,7 +44,44 @@ gsl_matrix* makerandCIJ_wd(int N, const gsl_vector* w) {
 	// CIJ(irp(1:K)) = w;
 	gsl_vector_view irp_subv = gsl_vector_subvector(irp, 0, K);
 	ordinal_index_assign(CIJ, &irp_subv.vector, w);
+	gsl_vector_free(w);
 	gsl_vector_free(irp);
 	
 	return CIJ;
+}
+
+/*
+ * Generates a random directed weighted graph with the same number of nodes,
+ * number of edges, and weight distribution as the given graph.  No edges are
+ * placed on the main diagonal.  The given matrix should therefore not contain
+ * nonzero entries on the main diagonal.
+ */
+gsl_matrix* bct::makerandCIJ_wd(const gsl_matrix* m) {
+	if (m->size1 != m->size2) throw size_exception();
+	int N = m->size1;
+	int K = N * N - N;
+	double* w = new double[K];
+	for (int i = 0, k = 0; i < (int)m->size1; i++) {
+		for (int j = 0; j < (int)m->size2; j++) {
+			if (i != j) {
+				w[k++] = gsl_matrix_get(m, i, j);
+			}
+		}
+	}
+	gsl_rng_default_seed = std::time(NULL);
+	gsl_rng* rng = gsl_rng_alloc(gsl_rng_default);
+	gsl_ran_shuffle(rng, w, K, sizeof(double));
+	gsl_rng_free(rng);
+	gsl_matrix* rand_m = gsl_matrix_alloc(m->size1, m->size2);
+	for (int i = 0, k = 0; i < (int)m->size1; i++) {
+		for (int j = 0; j < (int)m->size2; j++) {
+			if (i == j) {
+				gsl_matrix_set(rand_m, i, j, 0.0);
+			} else {
+				gsl_matrix_set(rand_m, i, j, w[k++]);
+			}
+		}
+	}
+	delete[] w;
+	return rand_m;
 }
