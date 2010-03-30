e69b99c65fa8218c699ce3107c6ba8b1701194fe
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-03-30 17:36:36 +0000

    Updated implementation of bct::mean to match MATLAB's.

--- utility.cpp
+++ utility.cpp
@@ -1,4 +1,6 @@
 #include "bct.h"
+#include <cmath>
+#include <cstring>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
@@ -33,15 +35,6 @@ void bct::init() {
 	gsl_set_error_handler(gsl_error_handler);
 }
 
-// TODO: This belongs in matlab/functions.cpp...move it when SWIG-accessible
-double bct::mean(const gsl_vector* v) {
-	double sum = 0.0;
-	for (int i = 0; i < (int)v->size; i++) {
-		sum += gsl_vector_get(v, i);
-	}
-	return sum / (double)v->size;
-}
-
 /*
  * Returns the number of edges in a directed graph.
  */
@@ -64,4 +57,54 @@ int bct::number_of_edges_und(const gsl_matrix* m) {
  */
 int bct::number_of_nodes(const gsl_matrix* m) {
 	return (int)m->size1;
+}
+
+// TODO: These belong in matlab/functions.cpp
+// Move them and remove unnecessary #includes when SWIG-accessible
+
+double bct::mean(const gsl_vector* v, const char* opt) {
+	if (std::strcmp(opt, "a") == 0) {
+		double sum = 0.0;
+		for (int i = 0; i < (int)v->size; i++) {
+			sum += gsl_vector_get(v, i);
+		}
+		return sum / (double)v->size;
+	} else if (std::strcmp(opt, "g") == 0) {
+		double product = 1.0;
+		for (int i = 0; i < (int)v->size; i++) {
+			product *= gsl_vector_get(v, i);
+		}
+		return std::pow(product, 1.0 / (double)v->size);
+	} else if (std::strcmp(opt, "h") == 0) {
+		double sum = 0.0;
+		for (int i = 0; i < (int)v->size; i++) {
+			sum += 1.0 / gsl_vector_get(v, i);
+		}
+		return (double)v->size / sum;
+	} else {
+		// TODO: Error message?
+		return 0.0;
+	}
+}
+
+gsl_vector* bct::mean(const gsl_matrix* m, int dim, const char* opt) {
+	if (dim == 1) {
+		gsl_vector* mean_v = gsl_vector_alloc(m->size2);
+		for (int i = 0; i < (int)m->size2; i++) {
+			gsl_vector_const_view m_col_i = gsl_matrix_const_column(m, i);
+			double value = mean(&m_col_i.vector, opt);
+			gsl_vector_set(mean_v, i, value);
+		}
+		return mean_v;
+	} else if (dim == 2) {
+		gsl_vector* mean_v = gsl_vector_alloc(m->size1);
+		for (int i = 0; i < (int)m->size1; i++) {
+			gsl_vector_const_view m_row_i = gsl_matrix_const_row(m, i);
+			double value = mean(&m_row_i.vector, opt);
+			gsl_vector_set(mean_v, i, value);
+		}
+		return mean_v;
+	} else {
+		return NULL;
+	}
 }
