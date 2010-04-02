32df176141cc01a4b6e07beabaf75e0cdbb7cdb4
stevencwilliams <stevencwilliams@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2010-04-02 19:26:03 +0000

    Prefer std::string to char*.

--- trunk/debug.cpp
+++ trunk/debug.cpp
@@ -3,14 +3,15 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_permutation.h>
 #include <gsl/gsl_vector.h>
+#include <string>
 
 /*
  * Prints a vector using the given format for each element.  This is only
  * provided for debugging purposes.  In other cases, use gsl_vector_fprintf.
  */
-void bct::printf(const gsl_vector* v, const char* format) {
+void bct::printf(const gsl_vector* v, const std::string& format) {
 	for (int i = 0; i < (int)v->size; i++) {
-		std::printf(format, gsl_vector_get(v, i));
+		std::printf(format.c_str(), gsl_vector_get(v, i));
 		std::printf(" ");
 	}
 	std::printf("\n");
@@ -20,10 +21,10 @@ void bct::printf(const gsl_vector* v, const char* format) {
  * Prints a matrix using the given format for each element.  This is only
  * provided for debugging purposes.  In other cases, use gsl_matrix_fprintf.
  */
-void bct::printf(const gsl_matrix* m, const char* format) {
+void bct::printf(const gsl_matrix* m, const std::string& format) {
 	for (int i = 0; i < (int)m->size1; i++) {
 		for (int j = 0; j < (int)m->size2; j++) {
-			std::printf(format, gsl_matrix_get(m, i, j));
+			std::printf(format.c_str(), gsl_matrix_get(m, i, j));
 			std::printf(" ");
 		}
 		std::printf("\n");
@@ -35,9 +36,9 @@ void bct::printf(const gsl_matrix* m, const char* format) {
  * provided for debugging purposes.  In other cases, use
  * gsl_permutation_fprintf.
  */
-void bct::printf(const gsl_permutation* p, const char* format) {
+void bct::printf(const gsl_permutation* p, const std::string& format) {
 	for (int i = 0; i < (int)p->size; i++) {
-		std::printf(format, gsl_permutation_get(p, i));
+		std::printf(format.c_str(), gsl_permutation_get(p, i));
 		std::printf(" ");
 	}
 	std::printf("\n");
