1c10e1256099a8fd4ed5acff67b6101c31bca2c0
michael.hallquist <michael.hallquist@e25b5de0-a7ce-11de-b2a4-832221a088ed>
2011-05-03 13:37:32 +0000

    Float version of degree, clustering_coef_bu, density. Parallelized degree and clustering_coef_bu. Fixed memory leak in efficiency.

--- density_und.cpp
+++ density_und.cpp
@@ -18,3 +18,18 @@ double bct::density_und(const gsl_matrix* CIJ) {
 	// kden = K/((N^2-N)/2);
 	return (double)K / ((double)(N * (N - 1)) / 2.0);
 }
+
+float bct::density_und(const gsl_matrix_float* CIJ) {
+	if (safe_mode) check_status(CIJ, SQUARE | UNDIRECTED, "density_und");
+	
+	// N = size(CIJ,1);
+	int N = CIJ->size1;
+	
+	// K = nnz(triu(CIJ));
+	gsl_matrix_float* triu_CIJ = triu(CIJ);
+	int K = nnz(triu_CIJ);
+	gsl_matrix_float_free(triu_CIJ);
+	
+	// kden = K/((N^2-N)/2);
+	return (float)K / ((float)(N * (N - 1)) / 2.0);
+}
